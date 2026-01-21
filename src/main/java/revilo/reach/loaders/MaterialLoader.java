90274c792ea4b57d33237dcbaf955086a4255ac5
Revilo126 <oli.facey5@gmail.com>
2026-01-21 16:35:53 +1100

    I hate spotless

--- src/main/java/revilo/reach/loaders/MaterialLoader.java
+++ src/main/java/revilo/reach/loaders/MaterialLoader.java
@@ -12,11 +12,27 @@ public class MaterialLoader implements Runnable {
 
     @Override
     public void run() {
-    	// Sr2Ru04 line //
-        RM.Mixer.addRecipe1( T, 512, 512, OM.dust(MT.OREMATS.Celestine), FL.Water.make(100), FL.CarbonDioxide.make(100), OM.dust(RCMT.SrCO3)); // too lazy to find h2s probs add later
+        // Sr2Ru04 line //
+        RM.Mixer.addRecipe1(
+            T,
+            512,
+            512,
+            OM.dust(MT.OREMATS.Celestine),
+            FL.Water.make(100),
+            FL.CarbonDioxide.make(100),
+            OM.dust(RCMT.SrCO3)); // too lazy to find h2s probs add later
         RM.Smelter.addRecipe1(T, 512, 2048, 10000, OM.dust(MT.Ru), FL.make("chlorine", 100), NF, OM.dust(RCMT.RuCl3));
         RM.Smelter.addRecipe1(T, 512, 2048, 10000, OM.dust(RCMT.RuCl3), FL.Water.make(250), NF, OM.dust(RCMT.RuO2));
-        RM.Smelter.addRecipe2(T, 2048, 1024, 10000, OM.dust(RCMT.SrCO3), OM.dust(RCMT.RuO2), FL.Helium.make(1000), FL.Helium.make(1000), OM.dust(RCMT.Sr2RuO4));
+        RM.Smelter.addRecipe2(
+            T,
+            2048,
+            1024,
+            10000,
+            OM.dust(RCMT.SrCO3),
+            OM.dust(RCMT.RuO2),
+            FL.Helium.make(1000),
+            FL.Helium.make(1000),
+            OM.dust(RCMT.Sr2RuO4));
     }
 
 }
